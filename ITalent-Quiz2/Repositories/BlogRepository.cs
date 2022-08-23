5bbcc3dd6ab65413a341ef0a52dfe7c0dda6bc1b
Serkan SAHIN <serkansahin114@gmail.com>
2022-08-23 09:08:54 +0300

    Repository oluşturuldu

---
+++ ITalent-Quiz2/Repositories/BlogRepository.cs
@@ -0,0 +1,49 @@
+﻿using ITalent_Quiz2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ITalent_Quiz2.Repositories
+{
+    public class BlogRepository : IBlogRepository
+    {
+        private readonly AppDbContext _context;
+
+        public BlogRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public void Create(Post post)
+        {
+            _context.Posts.Add(post);
+            _context.SaveChanges();
+        }
+
+        public Post Delete(int id)
+        {
+            var post = _context.Posts.Find(id);
+
+            _context.Posts.Remove(post);
+            _context.SaveChanges();
+            return post;
+        }
+
+        public List<Post> GetAllPost()
+        {
+            return _context.Posts.Include(x => x.Category).ToList();
+
+        }
+
+        public Post GetById(int id)
+        {
+            return _context.Posts.Include(x=> x.Category).First(x=>x.Id == id);
+        }
+
+        public List<Category> GetCategories()
+        {
+            return _context.Categories.ToList();
+        }
+
+   
+    }
+}
---
+++ ITalent-Quiz2/Repositories/IBlogRepository.cs
@@ -0,0 +1,18 @@
+﻿using ITalent_Quiz2.Models;
+
+namespace ITalent_Quiz2.Repositories
+{
+    public interface IBlogRepository
+    {
+
+        List<Category> GetCategories();
+        void Create(Post post);
+
+        List<Post> GetAllPost();
+
+        Post GetById(int id);
+        Post Delete(int id);
+
+
+    }
+}
