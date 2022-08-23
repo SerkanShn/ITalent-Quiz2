e4f1504b7940d742e7ea81f4941ab313c66cb2bb
Serkan SAHIN <serkansahin114@gmail.com>
2022-08-23 09:07:05 +0300

    Index Sayfası css,html ve IAction metodu eklendi

--- ITalent-Quiz2/Controllers/HomeController.cs
+++ ITalent-Quiz2/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using ITalent_Quiz2.Models;
+﻿using AutoMapper;
+using ITalent_Quiz2.Models;
+using ITalent_Quiz2.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +9,28 @@ namespace ITalent_Quiz2.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBlogRepository _blogRepository;
+        private readonly IMapper _mapper;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IBlogRepository blogRepository, IMapper mapper)
         {
             _logger = logger;
+            _blogRepository = blogRepository;
+            _mapper = mapper;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int page=1, int pageSize=5)
         {
-            return View();
+            var postListViewModel = _mapper.Map<List<IndexPostViewModel>>(_blogRepository.GetAllPost().Skip((page-1)*pageSize).Take(pageSize).ToList());
+            var totalCount = _blogRepository.GetAllPost().Count;
+            var categories = _mapper.Map<List<CategoryViewModel>>(_blogRepository.GetCategories());
+
+
+            ViewBag.page = page;
+            ViewBag.pageSize = pageSize;
+
+
+            return View((postListViewModel,totalCount,categories));
         }
 
         public IActionResult Privacy()
---
+++ ITalent-Quiz2/Models/PostCreateViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITalent_Quiz2.Models
+{
+    public class PostCreateViewModel
+    {
+        [Required(ErrorMessage = "Lütfen kategori seçiniz")]
+        public int CategoryId { get; set; }
+
+        [StringLength(100, ErrorMessage = "İsim alanı en fazla 20 karakter olabilir")]
+        [Required(ErrorMessage = "İsim alanı boş olamaz")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Özet alanı boş olamaz")]
+        [StringLength(450, ErrorMessage = "İsim alanı en fazla 20 karakter olabilir")]
+        public string Summary { get; set; }
+
+
+        public string PostBanner { get; set; }
+
+        [Required(ErrorMessage = "İçerik alanı boş olamaz")]
+        public string PostContents { get; set; }
+
+        
+    }
+}
